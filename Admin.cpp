8d16a7196c3133d18caa437ef61881fd1297ac73
Elys-Neferia <qhuyxiivi@gmail.com>
2025-11-03 11:31:27 +0700

    Update

--- Admin.cpp
+++ Admin.cpp
@@ -1,5 +1,56 @@
 #include <iostream>
+#include "string"
+#include <limits>
 #include "Admin.h"
+#include "Game.h"
 
 using namespace std;
 
+Admin::Admin(GameInStore* gameStore) : store(gameStore)
+{
+    // Gán con trỏ store của class Admin bằng địa chỉ của kho game
+}
+
+// Hàm tiện ích để tránh lỗi trôi lệnh (cin và getline)
+void Admin::clearInputBuffer()
+{
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+void Admin::addGameToStore()
+{
+    string id, name;
+    double price;
+
+    cout << "--- Them Game Moi ---" << endl;
+    cout << "Nhap ID game: ";
+    cin >> id;
+    clearInputBuffer();
+    cout << "Nhap Ten game: ";
+    getline(cin, name); // Dùng getline để nhập tên có khoảng trắng
+    cout << "Nhap Gia game: ";
+    while(!(cin >> price)) { // Vòng lặp kiểm tra input
+        cout << "Gia khong hop le. Vui long nhap so: ";
+        cin.clear();
+        clearInputBuffer();
+    }
+    clearInputBuffer(); // Xóa bộ đệm sau khi nhập giá
+    // Tạo game mới và thêm vào kho thông qua con trỏ 'store'
+    Game newGame(name, id, price);
+    store->addGame(newGame);
+
+    cout << "==> Da them game '" << name << "' vao kho." << endl;
+}
+void Admin::revGameFromStore()
+{
+}
+
+void Admin::updGamePrice()
+{
+}
+
+void Admin::viewAllGames()
+{
+}
+void Admin::viewAnalytics()
+{
+}
--- Admin.h
+++ Admin.h
@@ -1,13 +1,29 @@
 #ifndef ADMIN_H_INCLUDED
 #define ADMIN_H_INCLUDED
 
+#pragma once 
 #include "Game.h"
+#include "GameInStore.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 class Admin
 {
+private :
+    GameInStore* store; //Con trỏ trỏ tới kho game
+    
+    void clearInputBuffer(); // Hàm tiện ích để xóa bộ đệm input
+
 public:
+    Admin(GameInStore* gameStore); 
+    
     void addGame();
     void revGame();
+    void updGame(); 
+    void viewAllGames(); // xem kho
+    void viewAnalytics(); //xem thống kê
 };
 
 #endif // ADMIN_H_INCLUDED
